58a2a75b464c678bc5d3c234da5fd0584cc76b3c
hannesdonel <mail@hannesdonel.de>
2021-06-16 21:55:10 +0200

    Add further functionality:
    - Add form validation that fires an error if you try to add an empty item
    - Color change of "Add" button when form input changes
    - Add item by hitting "Enter"
    - Change cursor style when dragging items in list

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,33 @@
 function newItem() {
   //1. Adding a new item to the list of items:
+  let header = document.getElementById("sticky-header");
+  let error = document.createElement("span");
   let li = document.createElement("li");
   let inputValue = document.getElementById("input").value;
   let text = document.createTextNode(inputValue);
+  error.classList.add("validationError");
+  error.innerText = "Please type your item to add!";
   li.appendChild(text);
-
+  
+  function clearInput() {
+    document.getElementById("input").value = "";
+  }
+  
   if (inputValue === "") {
-    alert("You must write something!");
+    header.appendChild(error);
   } else {
     let list = document.querySelector("#list");
     list.appendChild(li);
+    clearInput();
+    document.getElementById("button").style.backgroundColor = "#fc999b";
   }
+  
+  textField.addEventListener("input", function () {
+    let inputValue = document.getElementById("input").value;
+    if (inputValue !== "") {
+      header.removeChild(error);
+    }
+  });
 
   //2. Crossing out an item from the list of items:
   function crossOut() {
@@ -31,4 +48,31 @@ function newItem() {
   }
   // 4. Reordering the items:
   $("#list").sortable();
+  function holdItem() {
+    li.style.cursor = "grabbing";
+  }
+  function releaseItem() {
+    li.style.cursor = "grab";
+  }
+
+  li.addEventListener("mousedown", holdItem);
+  li.addEventListener("mouseup", releaseItem);
 }
+
+// Change "Add" button color on input
+
+let textField = document.querySelector("form");
+let addButton = document.getElementById("button");
+textField.addEventListener("input", function buttonActive() {
+  let inputValue = document.getElementById("input").value;
+  if (inputValue !== "") {
+    addButton.style.backgroundColor = "#ff5557";
+  } else {
+    addButton.style.backgroundColor = "#fc999b";
+  }
+});
+
+textField.addEventListener("submit", function (e) {
+  e.preventDefault();
+  newItem();
+});
