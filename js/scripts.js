05b0e6e3ff484f638fdf3b605199c3095f0bba00
hannesdonel <mail@hannesdonel.de>
2021-11-25 15:28:36 +0100

    Add animations and a second list for checked items to preserve cross device compability.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ const error = $( "<span class=\"validationError delete\"></span>" );
 header.append( error );
 const textField = $( "form" );
 const addButton = $( "#button" );
+const list = $( "#list" );
+const checkedList = $( "#checkedList" );
+
 let validation = false;
 
 // Input validation
@@ -28,37 +31,61 @@ textField.on( "input", () => {
 // Adding a new item to the list of items
 function newItem() {
   const inputValue = $( "#input" ).val();
-  const li = $( `<li>${inputValue}</li>` );
+  const li = $( `<li><span>${inputValue}<span></li>` );
 
   function clearInput() {
     $( "#input" ).val( "" );
   }
 
   if ( validation ) {
-    const list = $( "#list" );
+    li.addClass( "add" );
     list.append( li );
+    setTimeout( () => li.removeClass( "add" ), 250 );
     clearInput();
     $( "#button" ).css( "background-color", "#fc999b" );
+    validation = false;
   }
+
+  // Add delete button and checkbox to list entry
+  const crossOutButton = $( "<button>X</button>" );
+  crossOutButton.on( "click", deleteListItem );
+  const checkBox = $( "<input type=\"checkbox\" />" );
+  checkBox.on( "click", ( e ) => console.log( e.target.checked ) );
+  li.append( crossOutButton, checkBox );
 
   // Crossing out an item from the list of items
-  function crossOut() {
-    li.toggleClass( "strike" );
+  function crossOut( value ) {
+    if ( value ) {
+      li.addClass( "toCheckedList" );
+      setTimeout( () => {
+        checkedList.prepend( li );
+        li.removeClass( "toCheckedList" );
+      }, 1000 );
+      li.toggleClass( "strike" );
+    } else {
+      li.addClass( "fadeOut" );
+      setTimeout( () => {
+        list.prepend( li );
+        li.removeClass( "fadeOut" );
+        li.addClass( "toList" );
+      }, 250 );
+      setTimeout( () => {
+        li.removeClass( "toList" );
+      }, 1250 );
+      li.toggleClass( "strike" );
+    }
   }
-  li.on( "dblclick", crossOut );
-
-  // Add delete button to list entry
-  const crossOutButton = $( "<button>X</button>" );
-  li.append( crossOutButton );
-  crossOutButton.on( "click", deleteListItem );
+  checkBox.on( "click", ( e ) => crossOut( e.target.checked ) );
 
   // Delete item from list
   function deleteListItem() {
-    li.remove();
+    li.addClass( "remove" );
+    setTimeout( () => li.remove(), 500 );
   }
 
   // Reordering the items
-  $( "#list" ).draggable();
+  $( "#list" ).sortable();
+  $( "#checkedList" ).sortable();
   function holdItem() {
     li.css( "cursor", "grabbing" );
   }
